fa522a359f8f296c587446e5fd0cda1aa29f1618
Rob <bonusdiez@mail.ru>
2018-12-15 03:06:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from "react-bootstrap/lib/Grid";
+import Row from "react-bootstrap/lib/Row";
+import Col from "react-bootstrap/lib/Col";
+import styled from "styled-components";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <SayFullName name="Robert" surname="Aglyamov" link="https://bonusdiez.ru" />
+        <SayFullName name="Vasya" surname="Pupkin" link="https://vk.com" />
+        <SayFullName name="Oleg" surname="Nemanov" link="https://fb.com" />
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
+}
+// ниже вставляем наши функции
+
+function SayFullName(props) {
+  return (
+    <div>
+      <h2>Моё имя {props.name}, фамилия - {props.surname}</h2>
+      <a href={props.link}> Ссылка на мой профиль </a>
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// все имена функции с большой буквы
+// function Hello() {
+//   let phrase = "World"
+//   return (
+//     <h1>Hello {phrase}</h1>
+//   )
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function SayFullName(props) {
+//   return (
+//     <div>
+//     <h2>Моё имя {props.name}, фамилия - {props.surname}</h2>
+//     <a href={props.link}> Ссылка на мой профиль </a>
+//     </div>
+//   )
+// }
 
+// function MeetAll() {
+//   return <div>
+//       <SayFullName name="Robert" surname="Aglyamov" link="https://bonusdiez.ru" />
+//       <SayFullName name="Vasya" surname="Pupkin" link="https://vk.com" />
+//       <SayFullName name="Oleg" surname="Nemanov" link="https://fb.com" />
+//     </div>
+// }
+ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<MeetAll/>, document.getElementById("root"));
+// const element = <SayFullName name="Robert" surname="Aglyamov" link="https://bonusdiez.ru" />
+// ReactDOM.render(<Hello />, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById("root"));
+// ReactDOM.render(
+//   <SayFullName name="Robert" surname="Aglyamov" link="https://bonusdiez.ru" />,
+//   document.getElementById("root")
+// );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
